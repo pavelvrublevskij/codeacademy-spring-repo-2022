9134236b5a807ba90fa9e7fa1966ab7059fe85a2
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2022-04-20 21:44:14 +0300

    1. [DI example] DI example with Spring Context

--- di-example/src/main/java/lt/codeacademy/InternalMarksDao.java
+++ di-example/src/main/java/lt/codeacademy/InternalMarksDao.java
@@ -1,7 +1,10 @@
 package lt.codeacademy;
 
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 
+@Component
 public class InternalMarksDao implements MarksDao {
 
     @Override
--- di-example/src/main/java/lt/codeacademy/Main.java
+++ di-example/src/main/java/lt/codeacademy/Main.java
@@ -1,8 +1,23 @@
 package lt.codeacademy;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
 
     public static void main(String[] args) {
+        //simplePlainJavaDIExample();
+        diExampleWithSpring();
+    }
+
+    private static void simplePlainJavaDIExample() {
         System.out.println("Pazymiu vidurkis: " + new GradeService(new InternalMarksDao()).averageGrade());
+    }
+
+    private static void diExampleWithSpring() {
+        ApplicationContext springContext = new AnnotationConfigApplicationContext(SpringContextConfig.class);
+
+        GradeService gradeService = springContext.getBean(GradeService.class);
+        System.out.println("Pazymiu vidurkis: " + gradeService.averageGrade());
     }
 }
