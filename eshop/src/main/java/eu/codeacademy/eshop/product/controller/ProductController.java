d0d8d1979e5787b4b7e3f917c2924fc98a1522bb
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2022-04-21 21:46:34 +0300

    2. product model layer mapping with view layer and add new product (not full)

--- eshop/src/main/java/eu/codeacademy/eshop/product/controller/ProductController.java
+++ eshop/src/main/java/eu/codeacademy/eshop/product/controller/ProductController.java
@@ -1,13 +1,22 @@
 package eu.codeacademy.eshop.product.controller;
 
+import eu.codeacademy.eshop.product.model.Product;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProductController {
 
     @GetMapping("/products/open")
-    public String openCrateProductForm() {
+    public String openCrateProductForm(Model model) {
+        model.addAttribute("product", new Product());
+        return "product";
+    }
+
+    @PostMapping("/products/open")
+    public String createProduct(Model model, Product product) {
         return "product";
     }
 }
---
+++ eshop/src/main/java/eu/codeacademy/eshop/product/model/Product.java
@@ -0,0 +1,9 @@
+package eu.codeacademy.eshop.product.model;
+
+import lombok.Data;
+
+@Data
+public class Product {
+
+    private String name;
+}
