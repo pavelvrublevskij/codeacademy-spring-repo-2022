497ea8729fc0c9c97d9bf74d2a5a5f6895b906cc
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2022-04-21 19:47:28 +0300

    2. initial eshop spring boot project

---
+++ eshop/src/test/java/eu/codeacademy/eshop/EshopApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.codeacademy.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
