af9ab1629b551607bc6b023c7fab982ab3291cde
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2022-04-25 19:41:21 +0300

    3. rollback into version without Qualifier

--- eshop/src/main/java/eu/codeacademy/eshop/product/controller/ProductController.java
+++ eshop/src/main/java/eu/codeacademy/eshop/product/controller/ProductController.java
@@ -2,7 +2,7 @@ package eu.codeacademy.eshop.product.controller;
 
 import eu.codeacademy.eshop.product.model.Product;
 import eu.codeacademy.eshop.product.service.ProductService;
-import org.springframework.beans.factory.annotation.Qualifier;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,14 +10,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/products")
 public class ProductController {
 
     private final ProductService productService;
-
-    public ProductController(@Qualifier("productServiceMock") ProductService productService) {
-        this.productService = productService;
-    }
 
     @GetMapping
     public String openCrateProductForm(Model model) {
--- eshop/src/main/java/eu/codeacademy/eshop/product/service/ProductService.java
+++ eshop/src/main/java/eu/codeacademy/eshop/product/service/ProductService.java
@@ -1,11 +1,23 @@
 package eu.codeacademy.eshop.product.service;
 
 import eu.codeacademy.eshop.product.model.Product;
+import eu.codeacademy.eshop.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface ProductService {
-    void addProduct(Product product);
+@Service
+@RequiredArgsConstructor
+public class ProductService {
 
-    List<Product> getProducts();
+    private final ProductRepository productRepository;
+
+    public void addProduct(Product product) {
+        productRepository.save(product);
+    }
+
+    public List<Product> getProducts() {
+        return productRepository.getProducts();
+    }
 }
