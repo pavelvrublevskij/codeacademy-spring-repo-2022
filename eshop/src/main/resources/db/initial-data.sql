c84c358806967f0ea52f420271a3d7cdbe0926e5
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2022-04-26 20:11:11 +0300

    4. add initial DB data

---
+++ eshop/src/main/resources/db/initial-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO PRODUCT (id, product_id, name, quantity_in_stock, price, description)
+VALUES
+    (1, 'e4dbc123-a7c2-4bee-a519-e1b9ba991341', 'iPhone XM', 200, 1599.99, 'Naujasias iPhone'),
+    (2, 'e4dbc123-a7c2-4bee-a519-e1b9ba991342', 'Samsung SX', 50, 999.99, 'Pats geriausias telefonas');
