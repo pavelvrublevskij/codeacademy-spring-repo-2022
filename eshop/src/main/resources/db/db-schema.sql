0064cf6307df1a25a061ee8ec48813e75bec4501
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2022-04-26 20:04:37 +0300

    4. add initial DB schema

---
+++ eshop/src/main/resources/db/db-schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE PRODUCT (
+    id INT PRIMARY KEY,
+    product_id UUID NOT NULL,
+    name VARCHAR(20) NOT NULL,
+    quantity_in_stock INT NOT NULL,
+    price DECIMAL(20, 2) NOT NULL,
+    description VARCHAR(500) DEFAULT NULL
+);
