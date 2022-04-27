66b88e4e0cfb4b10e9364a216c2201c6868a861c
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2022-04-27 19:00:00 +0300

    4. add more data seeders

--- eshop/src/main/resources/db/initial-data.sql
+++ eshop/src/main/resources/db/initial-data.sql
@@ -1,4 +1,11 @@
 INSERT INTO PRODUCT (product_id, name, quantity_in_stock, price, description)
 VALUES
     ('e4dbc123-a7c2-4bee-a519-e1b9ba991341', 'iPhone XM', 200, 1599.99, 'Naujasias iPhone'),
-    ('e4dbc123-a7c2-4bee-a519-e1b9ba991342', 'Samsung SX', 50, 999.99, 'Pats geriausias telefonas');
+    ('e4dbc123-a7c2-4bee-a519-e1b9ba991342', 'Samsung SX', 50, 999.99, 'Pats geriausias telefonas'),
+    ('e4dbc123-a7c2-4bee-a519-e1b9ba991343', 'Big Mac', 20, 4.99, 'Big burger'),
+    ('e4dbc123-a7c2-4bee-a519-e1b9ba991344', 'Mac Chicken', 200, 2.99, 'Chincken burger'),
+    ('e4dbc123-a7c2-4bee-a519-e1b9ba991345', 'Chicken Nuggets', 150, 0.99, 'Small piece of chicken'),
+    ('e4dbc123-a7c2-4bee-a519-e1b9ba991346', 'Cola', 200, 19.99, 'Dark soda'),
+    ('e4dbc123-a7c2-4bee-a519-e1b9ba991347', 'Ice cream', 500, 2.99, 'Vanilla'),
+    ('e4dbc123-a7c2-4bee-a519-e1b9ba991348', 'Cheese Burger', 20, 2.49, 'Hamburger with chseese'),
+    ('e4dbc123-a7c2-4bee-a519-e1b9ba991349', 'Hamburger', 16, 1.99, 'Bun and meat');
