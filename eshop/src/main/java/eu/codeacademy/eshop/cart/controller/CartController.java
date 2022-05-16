158bced44b4d263b66a59bd0e1b180b11e2fbff0
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2022-05-16 19:41:45 +0300

    13. create session and save and retrieve some value from session

--- eshop/src/main/java/eu/codeacademy/eshop/cart/controller/CartController.java
+++ eshop/src/main/java/eu/codeacademy/eshop/cart/controller/CartController.java
@@ -1,15 +1,30 @@
 package eu.codeacademy.eshop.cart.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/cart")
+@SessionAttributes("cart")
 public class CartController {
 
     @GetMapping
-    public String openCart() {
+    public String openCart(HttpServletRequest request, Model model) {
+        String cartSessionObjectValue = (String) request.getSession().getAttribute("cart");
+        model.addAttribute("cartValue", cartSessionObjectValue);
+
         return "/cart/cart";
+    }
+
+    @GetMapping("/add")
+    public String addToCart(Model model) {
+        model.addAttribute("cart", "Labas");
+
+        return "redirect:/cart";
     }
 }
