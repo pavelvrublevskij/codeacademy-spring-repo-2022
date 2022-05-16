55dd41e46cc954d16df8c667bf4bcbf114ee8a5a
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2022-05-16 19:49:57 +0300

    13. create session (@SessionAttributes) with model attribute (@ModelAttribute) cartSession

--- eshop/src/main/java/eu/codeacademy/eshop/cart/controller/CartController.java
+++ eshop/src/main/java/eu/codeacademy/eshop/cart/controller/CartController.java
@@ -3,27 +3,23 @@ package eu.codeacademy.eshop.cart.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
 @RequestMapping("/cart")
-@SessionAttributes("cart")
+@SessionAttributes("cartSession")
 public class CartController {
 
     @GetMapping
-    public String openCart(HttpServletRequest request, Model model) {
-        String cartSessionObjectValue = (String) request.getSession().getAttribute("cart");
-        model.addAttribute("cartValue", cartSessionObjectValue);
-
+    public String openCart(@ModelAttribute("cartSession") String cart) {
         return "/cart/cart";
     }
 
     @GetMapping("/add")
     public String addToCart(Model model) {
-        model.addAttribute("cart", "Labas");
+        model.addAttribute("cartSession", "Labas");
 
         return "redirect:/cart";
     }
