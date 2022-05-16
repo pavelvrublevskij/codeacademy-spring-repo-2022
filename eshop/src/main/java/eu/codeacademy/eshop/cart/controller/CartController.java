4fd95a0e114b5e76fec974a7cdcb178865af6f3e
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2022-05-16 19:54:55 +0300

    13. add @ModelAttribute to create session as default when no session exists

--- eshop/src/main/java/eu/codeacademy/eshop/cart/controller/CartController.java
+++ eshop/src/main/java/eu/codeacademy/eshop/cart/controller/CartController.java
@@ -1,7 +1,6 @@
 package eu.codeacademy.eshop.cart.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +11,20 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 @SessionAttributes("cartSession")
 public class CartController {
 
+    @ModelAttribute("cartSession")
+    public String createCart() {
+        return "Labas";
+    }
+
     @GetMapping
     public String openCart(@ModelAttribute("cartSession") String cart) {
         return "/cart/cart";
     }
 
-    @GetMapping("/add")
+/*    @GetMapping("/add")
     public String addToCart(Model model) {
         model.addAttribute("cartSession", "Labas");
 
         return "redirect:/cart";
-    }
+    }*/
 }
