5aa7396cb63f1e3c5d3ffde3d49119e8d632ebfe
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2022-05-16 20:53:29 +0300

    13. make cart template with hardcoded from controller product dto data (temp data)

--- eshop/src/main/java/eu/codeacademy/eshop/cart/controller/CartController.java
+++ eshop/src/main/java/eu/codeacademy/eshop/cart/controller/CartController.java
@@ -1,10 +1,13 @@
 package eu.codeacademy.eshop.cart.controller;
 
+import eu.codeacademy.eshop.product.dto.ProductDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/cart")
@@ -12,12 +15,15 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 public class CartController {
 
     @ModelAttribute("cartSession")
-    public String createCart() {
-        return "Labas";
+    public List<ProductDto> createCart() {
+//        return Collections.emptyList();
+        return List.of(ProductDto.builder()
+                        .name("Testas")
+                .build());
     }
 
     @GetMapping
-    public String openCart(@ModelAttribute("cartSession") String cart) {
+    public String openCart(@ModelAttribute("cartSession") List<ProductDto> cart) {
         return "/cart/cart";
     }
 
