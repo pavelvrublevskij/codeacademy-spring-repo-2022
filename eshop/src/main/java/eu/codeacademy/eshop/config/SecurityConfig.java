f24dee8b5ff57f23de0612829b1a1088d6265c91
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2022-05-18 21:32:32 +0300

    15. fix h2 access using WebSecurity configuration

--- eshop/src/main/java/eu/codeacademy/eshop/config/SecurityConfig.java
+++ eshop/src/main/java/eu/codeacademy/eshop/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package eu.codeacademy.eshop.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -11,7 +13,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/css/**", "/h2/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -21,18 +22,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginProcessingUrl("/login-eshop")
                 .defaultSuccessUrl("/", true)
                 .usernameParameter("loginEmail")
-                .passwordParameter("loginPassword")
-                .and()
-            .csrf()
-                .ignoringAntMatchers("/h2/**")
-                .and()
-            .headers()
-                .frameOptions()
-                .sameOrigin();
+                .passwordParameter("loginPassword");
     }
 
-/*    @Override
+    @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-    }*/
+        web.ignoring().requestMatchers(
+                PathRequest.toStaticResources().atCommonLocations(),
+                PathRequest.toH2Console()
+        );
+    }
 }
