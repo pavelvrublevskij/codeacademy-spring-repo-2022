fe128258ed5dfa4ec120cdb410871761fdd6e003
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2022-05-24 20:28:53 +0300

    18. use profile activation through yml configuration

--- eshop/src/main/java/eu/codeacademy/eshop/EshopApplication.java
+++ eshop/src/main/java/eu/codeacademy/eshop/EshopApplication.java
@@ -2,21 +2,12 @@ package eu.codeacademy.eshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.StandardEnvironment;
 
 @SpringBootApplication
 public class EshopApplication {
 
     public static void main(String[] args) {
-        ConfigurableEnvironment envConf = new StandardEnvironment();
-        envConf.addActiveProfile("dev");
-
-        SpringApplication app = new SpringApplication(EshopApplication.class);
-        app.setEnvironment(envConf);
-        app.run(args);
-
-        //SpringApplication.run(EshopApplication.class, args);
+       SpringApplication.run(EshopApplication.class, args);
     }
 
 }
