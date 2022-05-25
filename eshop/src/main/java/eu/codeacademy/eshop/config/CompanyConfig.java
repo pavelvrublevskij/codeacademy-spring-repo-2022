baa60bfb6a913f8a19c29d6f1594e4d8e8b7db42
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2022-05-25 18:38:56 +0300

    19. use custom property file

--- eshop/src/main/java/eu/codeacademy/eshop/config/CompanyConfig.java
+++ eshop/src/main/java/eu/codeacademy/eshop/config/CompanyConfig.java
@@ -3,9 +3,11 @@ package eu.codeacademy.eshop.config;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @Getter
+@PropertySource("classpath:company.properties")
 public class CompanyConfig {
 
     @Value("${eshop.company.name}")
