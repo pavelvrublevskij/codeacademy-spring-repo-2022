a96f9d4d998900923c16957808ed5608c0d2366a
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2022-05-30 19:49:55 +0300

    21. turn off integration test

--- eshop/src/test/java/eu/codeacademy/eshop/EshopApplicationTests.java
+++ eshop/src/test/java/eu/codeacademy/eshop/EshopApplicationTests.java
@@ -1,12 +1,9 @@
 package eu.codeacademy.eshop;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+//@SpringBootTest
 class EshopApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
