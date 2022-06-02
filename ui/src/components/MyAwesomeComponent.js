8f0134c13246200ca5d7236323e4507d8a1c321a
Tadas Simonavičius <tadas@inventi.lt>
2022-06-02 20:53:41 +0300

    24: converted function component to class

--- ui/src/components/MyAwesomeComponent.js
+++ ui/src/components/MyAwesomeComponent.js
@@ -1,2 +1,11 @@
+import React from "react";
 
-export default (props) => <h1>Hello, {props.name}!</h1>
+export default class MyAwesomeComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <h1>Hello, {this.props.name}!</h1>
+    }
+}
