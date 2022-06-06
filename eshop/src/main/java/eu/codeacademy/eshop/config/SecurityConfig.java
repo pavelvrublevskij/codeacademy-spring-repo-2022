416a5b421cc3e6040f768063c3a9a7d7de4cb551
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2022-06-06 20:36:06 +0300

    25. rest api intro

--- eshop/src/main/java/eu/codeacademy/eshop/config/SecurityConfig.java
+++ eshop/src/main/java/eu/codeacademy/eshop/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/public/**", "/").permitAll()
+                .antMatchers("/public/**", "/", "/api/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
