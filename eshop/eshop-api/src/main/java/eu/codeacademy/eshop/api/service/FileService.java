1dfa0438fd6b82cd0fe5043a6313e928ede30478
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2022-06-08 19:33:39 +0300

    27. upload file implementation

--- eshop/eshop-api/src/main/java/eu/codeacademy/eshop/api/service/FileService.java
+++ eshop/eshop-api/src/main/java/eu/codeacademy/eshop/api/service/FileService.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Service
 @Slf4j
@@ -16,6 +17,18 @@ public class FileService {
     private final Path fileLocation = Paths.get("./files").toAbsolutePath().normalize();
 
     public void saveFile(MultipartFile file) {
+        createDirectory();
+
+        try {
+            Path filePathWithFileName = fileLocation.resolve(file.getOriginalFilename());
+            Files.copy(file.getInputStream(), filePathWithFileName , StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            log.error("Cannot create file", e);
+            e.printStackTrace();
+        }
+    }
+
+    private void createDirectory() {
         try {
             if (!Files.exists(fileLocation)) {
                 Files.createDirectory(fileLocation);
