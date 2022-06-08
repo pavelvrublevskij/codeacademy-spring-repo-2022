79f0968c241f513cfe088118cd1d84d59bd0077d
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2022-06-08 19:04:58 +0300

    27. add empty file controller and service with max file size config

---
+++ eshop/eshop-api/src/main/java/eu/codeacademy/eshop/api/controller/FileController.java
@@ -0,0 +1,21 @@
+package eu.codeacademy.eshop.api.controller;
+
+import eu.codeacademy.eshop.api.service.FileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+public class FileController {
+
+    private final FileService fileService;
+
+    @PostMapping("/api/file/upload")
+    public void saveFile(@RequestParam MultipartFile file) {
+        String a;
+        //
+    }
+}
---
+++ eshop/eshop-api/src/main/java/eu/codeacademy/eshop/api/service/FileService.java
@@ -0,0 +1,9 @@
+package eu.codeacademy.eshop.api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileService {
+
+
+}
