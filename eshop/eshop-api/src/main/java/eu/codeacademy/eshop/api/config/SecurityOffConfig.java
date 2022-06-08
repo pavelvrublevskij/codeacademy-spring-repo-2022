907052420f52ee8ba42a91d915d116c072cb6bc1
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2022-06-08 20:50:15 +0300

    --- !!!! mvc module removed !!!!

--- eshop/eshop-api/src/main/java/eu/codeacademy/eshop/api/config/SecurityOffConfig.java
+++ eshop/eshop-api/src/main/java/eu/codeacademy/eshop/api/config/SecurityOffConfig.java
@@ -1,4 +1,4 @@
-package eu.codeacademy.eshop.config;
+package eu.codeacademy.eshop.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- eshop/eshop-main/src/main/resources/static/js/language.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $("#languageSelector").change(function () {
-        let selectedOption = $('#languageSelector').val();
-        if (selectedOption !== ''){
-            window.location.replace('?lang=' + selectedOption);
-        }
-    });
-})
