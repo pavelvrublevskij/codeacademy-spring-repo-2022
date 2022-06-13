dc5ec7ea7d099bd4b45483bd1c76d21fa566e1c3
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2022-06-13 19:55:36 +0300

    28. move product openapi annotations into interface

--- eshop/eshop-api/src/main/java/eu/codeacademy/eshop/api/controller/ProductApiController.java
+++ eshop/eshop-api/src/main/java/eu/codeacademy/eshop/api/controller/ProductApiController.java
@@ -3,93 +3,49 @@ package eu.codeacademy.eshop.api.controller;
 import eu.codeacademy.eshop.api.dto.ProductsResponse;
 import eu.codeacademy.eshop.common.product.dto.ProductDto;
 import eu.codeacademy.eshop.common.product.service.ProductService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(ProductApiController.PRODUCTS_ROOT_PATH)
-@Api(tags = "Product Controller")
-public class ProductApiController {
-
-    public static final String PRODUCTS_ROOT_PATH = "/products";
-    private static final String UUID_PATH = "/{uuid}";
+public class ProductApiController implements ProductApiSpec {
 
     private final ProductService productService;
 
-    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    @ApiOperation(
-            value = "Get all product",
-            notes = "Get all product from db, and any other information could be here")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Kai sekmingai grazina produktus"),
-            @ApiResponse(code = 401, message = "Reikalauja prisijungimo gaunant produktu sarasa"),
-            @ApiResponse(code = 403, message = "Neturite reikalingu teisiu gauti produktu sarasa")
-    })
     public ProductsResponse getProducts() {
         return ProductsResponse.builder()
                 .products(productService.getProducts())
                 .build();
     }
 
-    @GetMapping("/page")
-    public Page<ProductDto> getProductsPaginated(@RequestParam("page") int page, @RequestParam("size") int size) {
+    public Page<ProductDto> getProductsPaginated(int page, int size) {
         return productService.getProductPaginated(PageRequest.of(page, size));
     }
 
-    @GetMapping(
-            path = UUID_PATH,
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    @ApiOperation(value = "Get one product by id")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Kai sekmingai grazina produkta"),
-            @ApiResponse(code = 401, message = "Reikalauja prisijungimo gaunant produkta"),
-            @ApiResponse(code = 403, message = "Neturite reikalingu teisiu gauti produkta")
-    })
-    public ProductsResponse getProducts(@PathVariable("uuid") UUID uuid) {
+    public ProductsResponse getProducts(UUID uuid) {
         return ProductsResponse.builder()
                 .products(List.of(productService.getProductByUUID(uuid)))
                 .build();
     }
 
-    @DeleteMapping(path = UUID_PATH)
-    @ApiOperation(value = "Delete product", httpMethod = "DELETE")
-    public void deleteProduct(@PathVariable("uuid") UUID productId) {
+    public void deleteProduct(UUID productId) {
         productService.deleteProduct(productId);
     }
 
-    @PostMapping
-    @ApiOperation(value = "Create product", httpMethod = "POST")
-    public ResponseEntity<Void> createProduct(@Valid @RequestBody ProductDto productDto) {
+    public ResponseEntity<Void> createProduct(ProductDto productDto) {
         productService.addProduct(productDto);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @PutMapping
-    @ApiOperation(value = "Update product", httpMethod = "PUT")
-    public ResponseEntity<Void> updateProduct(@Valid @RequestBody ProductDto productDto) {
+    public ResponseEntity<Void> updateProduct(ProductDto productDto) {
         if (productService.updateProduct(productDto)) {
             return ResponseEntity.status(HttpStatus.CREATED).build();
         }
---
+++ eshop/eshop-api/src/main/java/eu/codeacademy/eshop/api/controller/ProductApiSpec.java
@@ -0,0 +1,84 @@
+package eu.codeacademy.eshop.api.controller;
+
+import eu.codeacademy.eshop.api.dto.ProductsResponse;
+import eu.codeacademy.eshop.common.product.dto.ProductDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.data.domain.Page;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+import java.util.UUID;
+
+@RequestMapping(ProductApiSpec.PRODUCTS_ROOT_PATH)
+@Api(tags = "Product Controller")
+public interface ProductApiSpec {
+
+    String PRODUCTS_ROOT_PATH = "/products";
+    String UUID_PATH = "/{uuid}";
+
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @ApiOperation(
+            value = "Get all product",
+            notes = "Get all product from db, and any other information could be here")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Kai sekmingai grazina produktus"),
+            @ApiResponse(code = 401, message = "Reikalauja prisijungimo gaunant produktu sarasa"),
+            @ApiResponse(code = 403, message = "Neturite reikalingu teisiu gauti produktu sarasa")
+    })
+    ProductsResponse getProducts();
+
+    @GetMapping("/page")
+    @ApiOperation(value = "Get all product by page and size")
+    Page<ProductDto> getProductsPaginated(
+            @ApiParam(
+                    name =  "page",
+                    type = "int",
+                    value = "Number of page",
+                    example = "1",
+                    required = true)
+            @RequestParam("page") int page,
+
+            @ApiParam(
+                    name =  "size",
+                    type = "int",
+                    value = "Content size in page",
+                    example = "1",
+                    required = true)
+            @RequestParam("size") int size);
+
+    @GetMapping(
+            path = UUID_PATH,
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @ApiOperation(value = "Get one product by id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Kai sekmingai grazina produkta"),
+            @ApiResponse(code = 401, message = "Reikalauja prisijungimo gaunant produkta"),
+            @ApiResponse(code = 403, message = "Neturite reikalingu teisiu gauti produkta")
+    })
+    ProductsResponse getProducts(@PathVariable("uuid") UUID uuid);
+
+    @DeleteMapping(path = UUID_PATH)
+    @ApiOperation(value = "Delete product", httpMethod = "DELETE")
+    void deleteProduct(@PathVariable("uuid") UUID productId);
+
+    @PostMapping
+    @ApiOperation(value = "Create product", httpMethod = "POST")
+    ResponseEntity<Void> createProduct(@Valid @RequestBody ProductDto productDto);
+
+    @PutMapping
+    @ApiOperation(value = "Update product", httpMethod = "PUT")
+    ResponseEntity<Void> updateProduct(@Valid @RequestBody ProductDto productDto);
+}
