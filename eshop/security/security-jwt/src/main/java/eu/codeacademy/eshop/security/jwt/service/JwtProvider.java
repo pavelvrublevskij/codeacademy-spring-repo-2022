5e8dc502fcccb03f4913db2cff5759149ad537f6
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2022-06-15 20:58:09 +0300

    30. [RestApi] - create JWT token when login (authenticate)

--- eshop/security/security-jwt/src/main/java/eu/codeacademy/eshop/security/jwt/service/JwtProvider.java
+++ eshop/security/security-jwt/src/main/java/eu/codeacademy/eshop/security/jwt/service/JwtProvider.java
@@ -1,12 +1,39 @@
 package eu.codeacademy.eshop.security.jwt.service;
 
 import eu.codeacademy.eshop.security.jwt.dto.UserRoleDto;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtProvider {
 
+    private final static Date NOW = new Date();
+
+    @Value("#{${security.jwt.validity-time} * 60 * 1000}")
+    private long tokenValidityInMillis;
+
+    @Value("${security.jwt.secret-key}")
+    private byte[] secretKey;
+
     public String getToken(UserRoleDto principal) {
-        return "My token: " + principal.getFullName();
+        return Jwts.builder()
+                .setHeaderParam("typ", "JWT")
+                .setIssuer("eshop-api")
+                .setAudience("eshop-ui")
+                .setSubject(principal.getUsername())
+                .setIssuedAt(NOW)
+                .setExpiration(new Date(NOW.getTime() + tokenValidityInMillis))
+                .claim("roles", principal.getAuthorities().stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.toSet()))
+                .signWith(Keys.hmacShaKeyFor(secretKey), SignatureAlgorithm.HS512)
+                .compact();
     }
 }
