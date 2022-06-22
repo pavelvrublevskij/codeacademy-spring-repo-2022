778ddf1c7d3682923f5d9c6cb3363fbdc9853219
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2022-06-22 21:49:23 +0300

    32. [RestApi] - add security reference and scheme to swagger

--- eshop/eshop-api/src/main/java/eu/codeacademy/eshop/api/config/SwaggerConfig.java
+++ eshop/eshop-api/src/main/java/eu/codeacademy/eshop/api/config/SwaggerConfig.java
@@ -3,24 +3,50 @@ package eu.codeacademy.eshop.api.config;
 import eu.codeacademy.eshop.commons.swagger.annotation.OpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.OAS_30)
+        return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(getInfo())
                 .select()
                     .apis(RequestHandlerSelectors.withClassAnnotation(OpenApi.class))
+                .build()
+                .securitySchemes(List.of(jwtAuthScheme()))
+                .securityContexts(List.of(jwtAuthContext()));
+    }
+
+    private SecurityContext jwtAuthContext() {
+        return SecurityContext.builder()
+                .securityReferences(jwtAuthReference())
                 .build();
+    }
+
+    private List<SecurityReference> jwtAuthReference() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Collections.singletonList(new SecurityReference("JWT", authorizationScopes));
+    }
+
+    private SecurityScheme jwtAuthScheme() {
+        return new ApiKey("JWT", HttpHeaders.AUTHORIZATION, "header");
     }
 
     private static ApiInfo getInfo() {
