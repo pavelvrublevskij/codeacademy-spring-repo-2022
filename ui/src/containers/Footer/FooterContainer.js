ad411185f789239496d85498059016bcc2beb249
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2022-06-27 20:46:37 +0300

    34. [React] implement Footer

--- ui/src/containers/Footer/FooterContainer.js
+++ ui/src/containers/Footer/FooterContainer.js
@@ -1,3 +1,10 @@
-const FooterContainer = () => <span>Footer</span>;
+import { Container } from 'react-bootstrap';
+
+const FooterContainer = () =>
+    <footer>
+        <Container fluid={'sm'}>
+            <span className="text-muted">CodeAcademy. Spring MVC. All Rights reserved </span>
+        </Container>
+    </footer>;
 
 export default FooterContainer;
