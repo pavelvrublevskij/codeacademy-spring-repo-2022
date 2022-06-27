fdc2e378a382ca3c2203ef3e282d427fd136c757
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2022-06-27 18:30:30 +0300

    34. [React E-Shop init] remove not necessary files (clean up)

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,32 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import MyAwesomeComponent from "./components/MyAwesomeComponent";
-import ButtonWithText from "./components/ButtonWithText";
-import ButtonWithTextWithHooks from "./components/ButtonWithTextWithHooks";
-import Product from "./components/Product";
-
 function App() {
   return (
-    <div className="App"><header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <MyAwesomeComponent name="World"/>
-        <ButtonWithText text="Nebuvo paspaustas"/>
-        <hr/>
-        <ButtonWithTextWithHooks text="Naujas"/>
-        <hr/>
-        <Product/>
-      </header>
+    <div>
+      Hello, 101-4 Folks!
     </div>
   );
 }
