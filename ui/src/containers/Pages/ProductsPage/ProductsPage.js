bcaea73b4fdf7276778f7a2067240545a76ba605
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2022-06-29 19:50:29 +0300

    36. [React] display products using bootstrap cards

--- ui/src/containers/Pages/ProductsPage/ProductsPage.js
+++ ui/src/containers/Pages/ProductsPage/ProductsPage.js
@@ -1,18 +1,42 @@
 import { getProductsApi } from '../../../api/apiEndpoints';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 
 const ProductsPage = () => {
 
+    const [productItems, setProductItems] = useState([])
+
     useEffect(() => {
         getProductsApi()
-            .then((response) => console.log('response', response))
+            .then(({data}) => {
+                console.log(data)
+                setProductItems(data.products)
+            })
             .catch((error) => console.log('error', error))
     })
 
     return (
-        <>
-            <h1>Product list</h1>
-        </>
+        <Container fluid>
+            <h1 className={"text-center"}>Product list</h1>
+            <Row xs={1} md={2} className="g-4">
+                {productItems.map(product => (
+                    <Col key={product.productId}>
+                        <Card>
+                            <Card.Header>
+                                <div className="d-flex justify-content-around">
+                                    <div>{product.price} Eur</div>
+                                    <div>{product.quantity} available</div>
+                                </div>
+                            </Card.Header>
+                            <Card.Body>
+                                <Card.Title>{product.name}</Card.Title>
+                                <Card.Text>{product.description}</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
     )
 }
 
