1730c33fe661ceb70c5a820d67671d251002defc
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2022-06-29 20:25:59 +0300

    36. [React] add loading spinner and make request process as async using Promise

--- ui/src/api/apiEndpoints.js
+++ ui/src/api/apiEndpoints.js
@@ -1,6 +1,14 @@
 import HTTP from './axiosConfig';
 
-const getProductsApi = () => HTTP.get("/products")
+const getProductsApi = () =>
+    HTTP.get("/products")
+        .finally(response =>
+            new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve(response);
+                }, 2000);
+            })
+        )
 
 const createProductApi = (productData) => HTTP.post("/products", productData)
 
--- ui/src/containers/Pages/ProductsPage/ProductsPage.js
+++ ui/src/containers/Pages/ProductsPage/ProductsPage.js
@@ -1,42 +1,47 @@
 import { getProductsApi } from '../../../api/apiEndpoints';
 import { useEffect, useState } from 'react';
-import { Card, Col, Container, Row } from 'react-bootstrap';
+import { Card, Col, Container, Row, Spinner } from 'react-bootstrap';
 
 const ProductsPage = () => {
 
-    const [productItems, setProductItems] = useState([])
+    const [productItems, setProductItems] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         getProductsApi()
-            .then(({data}) => {
-                setProductItems(data.products)
+            .then(({ data }) => {
+                setProductItems(data.products);
             })
             .catch((error) => console.log('error', error))
-    }, [])
+            .finally(() => setLoading(false));
+    }, []);
 
     return (
         <Container fluid>
-            <h1 className={"text-center"}>Product list</h1>
-            <Row xs={1} md={2} className="g-4">
-                {productItems.map(product => (
-                    <Col key={product.productId}>
-                        <Card>
-                            <Card.Header>
-                                <div className="d-flex justify-content-around">
-                                    <div>{product.price} Eur</div>
-                                    <div>{product.quantity} available</div>
-                                </div>
-                            </Card.Header>
-                            <Card.Body>
-                                <Card.Title>{product.name}</Card.Title>
-                                <Card.Text>{product.description}</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                ))}
+            <h1 className={'text-center'}>Product list</h1>
+            <Row xs={1} md={2} className='g-4'>
+                {loading
+                    ? <Spinner className="text-center" animation='border' />
+                    : productItems.map(product => (
+                        <Col key={product.productId}>
+                            <Card>
+                                <Card.Header>
+                                    <div className='d-flex justify-content-around'>
+                                        <div>{product.price} Eur</div>
+                                        <div>{product.quantity} available</div>
+                                    </div>
+                                </Card.Header>
+                                <Card.Body>
+                                    <Card.Title>{product.name}</Card.Title>
+                                    <Card.Text>{product.description}</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))
+                }
             </Row>
         </Container>
-    )
-}
+    );
+};
 
 export default ProductsPage;
