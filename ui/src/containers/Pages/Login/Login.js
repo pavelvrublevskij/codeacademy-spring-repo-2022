20122a0ae30e4ee678ca5aadce2103179b136a63
Tadas Simonavičius <tadas@inventi.lt>
2022-06-30 21:49:27 +0300

    37: add email simple validation

--- ui/src/containers/Pages/Login/Login.js
+++ ui/src/containers/Pages/Login/Login.js
@@ -6,6 +6,10 @@ const Login = () => {
         email: ''
     })
 
+    const [errors, setErrors] = useState({
+        email: ''
+    })
+
     const onSubmit = (e) => {
         e.preventDefault()
 
@@ -13,16 +17,41 @@ const Login = () => {
     }
 
     const onChange = (e) => {
-        setLogin({
-            email: e.target.value
+        const email = e.target.value
+
+        const errMessage = validateEmail(email)
+
+        setErrors({
+            email: errMessage
         })
+
+        setLogin({
+            email
+        })
+    }
+
+    const validateEmail = (email) => {
+
+        if (!email.includes("@")) {
+            return "CIA NE EMAIL'AS!!!";
+        }
+
+        return ''
     }
 
     return (
         <form onSubmit={onSubmit}>
-            <label>Email</label>
-            <input name="email" value={login.email} onChange={onChange}/>
-            <button type="submit">Submit</button>
+            <div>
+                <label>Email</label>
+                <input name="email" value={login.email} onChange={onChange}/>
+                {
+                    errors.email && <span color="red">{errors.email}</span>
+                }
+            </div>
+
+            <div>
+                <button type="submit">Submit</button>
+            </div>
         </form>
     )
 }
